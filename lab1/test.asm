65f0be44a1e4c8faf12f3690632a1d335bceda03
whi497 <1624745389@qq.com>
2021-12-20 15:59:29 +0800

    add

--- lab1/test.asm
+++ lab1/test.asm
@@ -1,21 +1,25 @@
-    .orig x3500     ; starting position
-    and r0, r0, #0  ; result
-    ld r1, m1       ; multiplicand
-    ld r2, m2       ; multiplier
-    add r3, r0, #1  ; mask for testing bit
-    add r4, r0, #-1 ; mask for end condition (all 1 bits)
-l1  and r2, r2, r4  ; any bits remaining?
-    brz l4          ; no, done
-    and r5, r2, r3  ; test bit
-    brz l2          ; if bit is zero, skip addition
-    add r0, r0, r1  ; add multiplicand to result
-l2  add r1, r1, r1  ; shift multiplicand bits left
-    add r3, r3, r3  ; shift test mask left
-    add r4, r4, r4  ; shift end mask left
-    brnzp l1        ; redo
-l4  st r0, m3
-    trap x25        ; end
-m1  .fill #5
-m2  .fill #3
-m3  .fill #0
-    .end
+.orig X3000
+            ADD R0  R0  #-1
+            BRP NEXT
+            ADD R7  R7  #1
+            HALT
+NEXT        LD  R3  NUM
+            ADD R7  R7  #2 
+            ADD R6  R6  #1
+            ADD R5  R5  #1
+LOOP        ADD R0  R0  #-1
+            BRP CONTINUE
+            HALT
+CONTINUE    ADD R4  R5  #0
+            ADD R5  R6  #0
+            ADD R6  R7  #0
+            ADD R7  R4  R6
+            ADD R7  R7  R4
+            AND R7  R7  R3
+            BRP LOOP
+NUM         .FILL x03FF
+            .FILL #930
+            .FILL #1
+            .FILL #1
+            .FILL #338
+.END
